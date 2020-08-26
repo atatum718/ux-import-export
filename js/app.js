addbfaacc88f5b420b9128792b6fb4536ca5f3d3
atatum718 <54254894+atatum718@users.noreply.github.com>
2020-08-25 23:44:11 -0400

    restored deleted files

---
+++ js/app.js
@@ -0,0 +1,24 @@
+import { Person, render } from "./personalProfile.js";
+
+let people = [
+  new Person("Bob", "imgs/bob-ross.jpg"),
+  new Person("Frida", "../imgs/frida-kahlo.jpg"),
+  new Person("Dali", "../imgs/dali.jpg"),
+];
+
+window.onload = () => {
+  renderList(people);
+};
+
+function renderList(people) {
+  let html = "";
+  html += `<ul>`;
+  for (let person of people) {
+    html += "<li>";
+    html += render(person);
+    html += "</li>";
+  }
+  html += "</ul>";
+
+  document.getElementById("list").innerHTML = html;
+}
